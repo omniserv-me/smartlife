a119337fcbe0c6becbaec917f8e0e7a77a27eeea
Leo Yesaulov <leo@omniserv.me>
2026-07-01 14:59:02 +0200

    upd env handling

--- src/data_handler.py
+++ src/data_handler.py
@@ -1,17 +1,20 @@
 import os
 from dotenv import load_dotenv, find_dotenv, set_key, get_key
 
+prod = os.environ.get("PROD") in {"true", "True", "TRUE"}
 # read and write both to the .env and environ for local debug purposes
 # tbh I was just too lazy to remove .env support
 class DataHandler:
     def __init__(self):
-        self.dotenv_file = find_dotenv()
-        load_dotenv(self.dotenv_file)
+        if not prod:
+            self.dotenv_file = find_dotenv()
+            load_dotenv(self.dotenv_file)
 
     def write(self, key: str, value: str):
         key=key.upper()
-        load_dotenv(self.dotenv_file)
-        set_key(self.dotenv_file, key, value)
+        if not prod:
+            load_dotenv(self.dotenv_file)
+            set_key(self.dotenv_file, key, value)
         os.environ[f"{key}"] = f"{value}"
 
     def get(self, key: str) -> str | None:
@@ -19,8 +22,10 @@ class DataHandler:
         result = self.get_fromOS(key)
         if result is not None:
             return result
-        load_dotenv(self.dotenv_file)
-        return get_key(self.dotenv_file, key)
+        if not prod:
+            load_dotenv(self.dotenv_file)
+            return get_key(self.dotenv_file, key)
+        return None
 
     def get_fromOS(self, key: str) -> str | None:
         return os.environ.get(f"{key}")
